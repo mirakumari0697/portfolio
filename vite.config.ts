dfcd7961776b6f5c285bb1411efea0d1bc917e71
mirakumari0697 <mira.kumari0697@gmail.com>
2026-04-19 03:17:56 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/secure-net-architect/" : "/",
+  base: mode === "production" ? "/portfolio/" : "/",
   server: {
     host: "::",
     port: 8080,
